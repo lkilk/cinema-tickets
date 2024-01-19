50cf80e40dc9a70ca4210ec1056128519b9026b0
Liam Kilkenny <liam.kilkenny@engineering.digital.dwp.gov.uk>
2024-01-19 10:53:04 +0000

    lint and testing set up complete

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -7,6 +7,15 @@ export default class TicketService {
    */
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+    try {
+      // check purchase request is valid
+      // calculate correct ticket cost
+      // make a payment request to `TicketPaymentService`
+      // calculate number of seats
+      // make a seat reservation request to `SeatReservationService`
+    } catch (error) {
+      // throws InvalidPurchaseException
+      throw new InvalidPurchaseException(`Invalid Purchase: ${error.message}`);
+    }
   }
 }
---
+++ cinema-tickets-javascript/test/lib/TicketTypeRequest.test.js
@@ -0,0 +1,27 @@
+import TicketTypeRequest from '../../src/pairtest/lib/TicketTypeRequest';
+
+describe('TicketTypeRequest', () => {
+  it('Should throw a TypeError for an invalid ticket type', () => {
+    expect(() => new TicketTypeRequest('INVALID', 1)).toThrow(TypeError);
+    expect(() => new TicketTypeRequest('INVALID', 1)).toThrow(
+      'type must be ADULT, CHILD, or INFANT',
+    );
+  });
+
+  it('Should throw a TypeError if noOfTickets is not an integer', () => {
+    expect(() => new TicketTypeRequest('ADULT', 'one')).toThrow(TypeError);
+    expect(() => new TicketTypeRequest('ADULT', 'one')).toThrow(
+      'noOfTickets must be an integer',
+    );
+  });
+
+  it('getNoOfTickets should return the correct number of tickets', () => {
+    const ticketRequest = new TicketTypeRequest('ADULT', 2);
+    expect(ticketRequest.getNoOfTickets()).toEqual(2);
+  });
+
+  it('getTicketType should return the correct ticket type', () => {
+    const ticketRequest = new TicketTypeRequest('ADULT', 2);
+    expect(ticketRequest.getTicketType()).toEqual('ADULT');
+  });
+});
